48d5b96234dfe989e33ee06dad9462ae245e8dbb
hyeyoung-Moon <mhy001419@gmail.com>
2026-02-18 01:19:13 +0900

    add init file and main file

---
+++ __main__.py
@@ -0,0 +1,6 @@
+from prac03 import *
+
+print(f'add(2+3) : {add(2, 3)}')
+print(f'subtraction(5-2) : {subtraction(5, 2)}')
+print(f'multiplication(3*4) : {multiplication(3, 4)}')
+print(f'division(10/2) : {division(10, 2)}')
